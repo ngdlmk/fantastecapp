d9129ffe8718e89abf06ccbe7275892093e7e365
EXT02D22329 <kamil.dogan@turkcell.consultant.com.tr>
2021-03-25 20:49:33 +0000

    project initiated

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.main}>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+});
+
+export default App;
