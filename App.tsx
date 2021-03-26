bd43f9fc8e8fc8ce311311b17d453e43256237f0
EXT02D22329 <kamil.dogan@turkcell.consultant.com.tr>
2021-03-26 01:31:08 +0000

    action sheet created and connected to main screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,82 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { useEffect } from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  FlatList
+} from 'react-native';
+import CommentContent from "./components/CommentContent";
+import data from "./data/fantastec.json";
+import KeyMomentSheet from "./components/KeyMomentsSheet"
+
+export interface KeyMoment {
+  id: Number,
+  minute: Number,
+  keyMomentType: String,
+  content: String
+}
+
+const renderCommentItem = ({ item, index }) => {
+  return (
+    <CommentContent
+      id={item.id}
+      content={item.content}
+      minute={item.minute}
+      keyMomentType={item.keyMomentType}
+      isLastItem={data.length - 1 === index ? true : false}
+      selected={true}
+    />
+  )
+}
 
 const App = () => {
   return (
-    <View style={styles.main}>
-    </View>
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.mainContainer}>
+        <View style={styles.commentContainer}>
+          <FlatList
+            data={data}
+            keyExtractor={item => item.id}
+            renderItem={renderCommentItem}
+          />
+        </View>
+        <View style={styles.sheetContainer}>
+          <KeyMomentSheet
+            header="Key Moments"
+            keyMoments={data.filter((moment: KeyMoment) => moment.keyMomentType !== null)}
+            onPress={(id: number) => alert(id)}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
+  safeAreaContainer: {
+    flex: 1
   },
+  mainContainer: {
+    flex: 1
+  },
+  commentContainer: {
+    flex: 1
+  },
+  sheetContainer: {
+    width: "100%",
+    height: 50,
+  }
 });
 
 export default App;
---
+++ components/KeyMomentsSheet.tsx
@@ -0,0 +1,95 @@
+import React, {useState} from 'react'
+import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native'
+import Animated, { withSpring, useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+export interface KeyMoment {
+  id: Number,
+  minute: Number,
+  keyMomentType: String,
+  content: String
+}
+
+export interface Props {
+  header: string
+  keyMoments: Array<KeyMoment>
+  onPress: Function 
+}
+
+const keyMomentSheet: React.FC<Props> = (props) => {
+  const [actionSheetStatus, setActionSheetStatus] = useState(false)
+  const offset = useSharedValue(0);
+  const status = useSharedValue(false)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: offset.value }],
+    };
+  });
+
+  const actionSheetHandler = () => {
+    setActionSheetStatus(!actionSheetStatus)
+    if (status.value)
+      status.value = false
+    else
+      status.value = true
+    offset.value = withSpring(status.value ? 0 : -150);
+  }
+
+
+  const renderActionSheetItem = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => handleActionSheetSelection(item.id)} style={{width: '100%', height: 40, flexDirection: 'row'}}>
+        <View style={{width: 70, height: '100%', alignItems: 'flex-end'}}>
+          <Text style={{fontSize: 20, fontWeight: '400'}}>{item.minute}</Text>
+        </View>
+        <View style={{flex: 1, marginLeft: 40}}>
+          <Text style={{fontSize: 20, fontWeight: '400'}}>{item.keyMomentType}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  const handleActionSheetSelection = (id: number) => {
+    actionSheetHandler()
+    props.onPress(id)
+  }
+
+  return (
+    <>
+      <Animated.View style={[styles.container, animatedStyles]}>
+        <View style={{ height: 60, width: '100%', flexDirection: 'row' }}>
+          <View style={{ width: 60, height: 60 }}></View>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 24, fontWeight: '700' }}>{props.header}</Text>
+          </View>
+          <View style={{ width: 60, height: 60, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity onPress={actionSheetHandler}>
+              <Icon name={actionSheetStatus ? 'chevron-down-circle-outline' : 'chevron-up-circle-outline'} size={32} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+          <View style={{flex: 1}}>
+            {
+              actionSheetStatus ? 
+              <FlatList
+                data={props.keyMoments}
+                keyExtractor={(item: KeyMoment) => item.id}
+                renderItem={renderActionSheetItem}
+              /> : null
+            }
+          </View>
+      </Animated.View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 200,
+    backgroundColor: 'white'
+  }
+});
+
+export default keyMomentSheet
